5d533fcb04d79ddd15f20073e4cc20bcef2c2546
Nicolas2595 <nmorelli10@gmail.com>
2021-05-28 15:39:21 +0200

    HeroComponent

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
     <TitleComponent />
+    <HeroComponent />
   </div>
 </template>
 
 <script>
 import TitleComponent from '@/components/TitleComponent.vue';
+import HeroComponent from './components/HeroComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    TitleComponent
+    TitleComponent,
+    HeroComponent
   }
 }
 </script>
 
-<style lang="scss">
+<style 
+    HeroComponentlang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/HeroComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+       <img src="../assets/logo.png" alt="vue-logo">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeroComponent"
+}
+</script>
